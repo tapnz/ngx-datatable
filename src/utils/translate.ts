0606a6be3016448138583773adf9c5ec2b973bb1
mikobiscante <michael@bbtdigital.com>
2019-04-17 09:30:26 +0800

    Removed checking of browsers before applying translate3d

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -6,12 +6,13 @@ const transform = typeof window !== 'undefined' ? getVendorPrefixedName('transfo
 const backfaceVisibility = typeof window !== 'undefined' ? getVendorPrefixedName('backfaceVisibility') : undefined;
 const hasCSSTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('transform') : undefined;
 const hasCSS3DTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('perspective') : undefined;
-const ua = typeof window !== 'undefined' ? window.navigator.userAgent : 'Chrome';
-const isSafari = (/Safari\//).test(ua) && !(/Chrome\//).test(ua);
+// const ua = typeof window !== 'undefined' ? window.navigator.userAgent : 'Chrome';
+// const isSafari = (/Safari\//).test(ua) && !(/Chrome\//).test(ua);
 
 export function translateXY(styles: any, x: number, y: number) {
   if (typeof transform !== 'undefined' && hasCSSTransforms) {
-    if (!isSafari && hasCSS3DTransforms) {
+    // if (!isSafari && hasCSS3DTransforms) {
+    if (hasCSS3DTransforms) {
       styles[transform] = `translate3d(${x}px, ${y}px, 0)`;
       styles[backfaceVisibility] = 'hidden';
     } else {
